53636d68de144461540779c93b3fae689334953e
Dale Hurd <110054852+dalehurd@users.noreply.github.com>
2022-11-22 11:36:27 -0500

    plot 5

--- Plot5.R
+++ Plot5.R
@@ -1,24 +1,25 @@
-#plot 4 
+#plot 5 
 #gather data
 
 SCCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
 NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
 
-#Select coal combustion_related sources using keywords from the Ei.Sector column in SCCC
+#Select Vehicle emission records
 
-data_coal <- SCCC[grepl("Comb.*Coal", SCCC$EI.Sector), ]
+Vehicle_records <- SCCC[grepl("Vehicle", SCCC$SCC.Level.Two), ]
 
+#Calculate total emissions from motor vehicle sources in Baltimore City
 
-#
-coal_scc <- unique(data_coal$SCC)  # Unique SCC rows
-coal_emi <- data[(NEI$SCC %in% coal_scc), ]  #cross reference to NEI data
-coal_year <- coal_emi %>% group_by(year) %>% summarise(total = sum(Emissions))  #summarize years
+Vehicle_records_unique <- unique(Vehicle_records$SCC)
+Vehicle_emission <- NEI[(NEI$SCC %in% Vehicle_records_unique), ]
+vehicle_aggregate <- Vehicle_emission %>% filter(fips == "24510") %>% group_by(year) %>% 
+  summarise(total = sum(Emissions))
 
-g <- ggplot(coal_year, aes(factor(year), total/1000, label = round(total/1000))) 
-g<- g + geom_col( fill = "purple")
-g <- g+  ggtitle("Total coal combustion related PM2.5 Emissions") 
-g <- g+  xlab("Year") + ylab("PM2.5 Emissions in Kilotons") 
-g <- g+  ylim(c(0, 620)) + theme_classic()+ geom_text(size = 5, vjust = -1)
-g <- g+  theme(plot.title = element_text(hjust = 0.5))
-g
+#Plot total emissions from motor vehicle sources in Baltimore City
 
+g <- ggplot(vehicle_aggregate, aes(factor(year), total, label = round(total))) 
+g <- g+  geom_col( fill = "purple") 
+g <- g+  ggtitle("Vehicle Emissions in Baltimore City") 
+g <- g+  xlab("Year") + ylab("PM2.5 Emissions in Tons")
+g <- g+  ylim(c(0, 450)) + theme_classic()+ geom_text(size = 5, vjust = -1) 
+g <- g+  theme(plot.title = element_text(hjust = 0.5))+  theme_classic()
