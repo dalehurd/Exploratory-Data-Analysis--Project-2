5022f7e030d03b45a85260b254205ad84d7f1cd9
Dale Hurd <110054852+dalehurd@users.noreply.github.com>
2022-11-22 12:29:20 -0500

    plot 6

---
+++ Plot6.R
@@ -0,0 +1,39 @@
+#plot 6 
+#gather data
+
+SCCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+
+#get Vehicle Emission data (reused from previous plot)
+Vehicle_records <- SCCC[grepl("Vehicle", SCCC$SCC.Level.Two), ]
+Vehicle_records_unique <- unique(Vehicle_records$SCC)
+Vehicle_emission <- NEI[(NEI$SCC %in% Vehicle_records_unique), ]
+
+#Select Baltimore City and Los Angeles County data
+balt <-Vehicle_emission %>% filter(fips == "24510")
+LA<-Vehicle_emission %>% filter(fips == "06037")
+
+#add Identifier and combine
+balt$location <- "Baltimore City"
+LA$location <- "Los Angeles County"
+combined_data <- rbind(balt,LA)
+  
+  
+  
+plot_data  <-  combined_data %>%  group_by(location, year) %>% summarise(total = sum(Emissions))
+
+#generate plot
+mycolors <- c("purple", "gold")
+
+g <- ggplot(plot_data, aes(factor(year), total, fill = location, label = round(total)))
+g <- g+  geom_col() + facet_grid(. ~ location)  
+g <- g+   ggtitle("Total Motor Vehicle Emissions") 
+g <  g+   scale_fill_manual(values=mycolors)
+g <- g+  xlab("Year") + ylab("Pm2.5 Emissions in Tons") 
+g <- g+  theme(plot.title = element_text(hjust = 0.5)) + ylim(c(0, 8000)) 
+g <- g+   theme_classic() + geom_text(size = 4, vjust = -1)
+g
+
+
+
